aa80ddc2d695a56ef5c094f3bbfc089565160503
DeniMalt <116885648+DeniMalt@users.noreply.github.com>
2024-11-25 23:39:41 +0600

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class URLValidator {
 
-    private static final String URL_REGEX = "^(https?:\\/\\/)?(?!-)([a-zA-Z]{2,}(?:\\.[a-zA-Z]{2,})+)(:[0-9]{1,5})?(\\/[\\S]*)?(#[\\S]*)?$";
+    private static final String URL_REGEX = "^(https?://)?(?!-)([a-zA-Z]{2,}(?:\\.[a-zA-Z]{2,})+)(:[0-9]{1,5})?(/\\S*)?(#\\S*)?$";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
